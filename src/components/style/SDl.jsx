e4547cc979a073d454b1ce6c2b8a2de5278e1a68
FujiiAmon <amondealwis@gmail.com>
2024-11-08 19:35:34 +0900

    デザインを軽く修正

--- src/components/style/SDl.jsx
+++ src/components/style/SDl.jsx
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 export const SDl = styled.dl`
-  color: dimgray;
-  background-color: whitesmoke;
-  border-radius: 10px;
-  text-align: left;
-  padding: 7px;
-  dt {
-    text-decoration: underline;
-  }
-  dd {
-    padding: 5px;
-  }
+    width: 100%;
+    color: dimgray;
+    background-color: whitesmoke;
+    border-radius: 10px;
+    text-align: left;
+    padding: 7px;
+    dt {
+        text-decoration: underline;
+    }
+    dd {
+        padding: 5px;
+    }
 `;
